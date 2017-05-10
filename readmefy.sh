dc1dfccde221464ed5660888e38b50a2e9f93fe5
Mr. Is <isaac.ardis@gmail.com>
2017-05-10 17:28:42 -0500

    fix datshi

--- readmefy.sh
+++ readmefy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 > README.md
-for file in ./*.jpg */*.jpg ./*.png */*.png; do
-    echo "![$file]($file)" >> README.md
+for file in *.jpg *.png  **/*.jpg **/*.png; do
+	if [[ -f $file ]]; then
+    	echo "![$file]($file)" >> README.md
+	fi
 done
